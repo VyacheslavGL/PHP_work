736ab426eb899dff824b941ec403d616366fc6ec
VyacheslavGL <48326907+VyacheslavGL@users.noreply.github.com>
2019-05-27 21:53:20 +0300

    add 27.05

---
+++ tasks.php
@@ -0,0 +1,38 @@
+<?php
+
+//Отсортировать массив по 'price'
+$arr = [
+    '1' => [
+        'price' => 10,
+        'count' => 2
+    ],
+    '2' => [
+        'price' => 5,
+        'count' => 5
+    ],
+    '3' => [
+        'price' => 8,
+        'count' => 5
+    ],
+    '4' => [
+        'price' => 12,
+        'count' => 4
+    ],
+    '5' => [
+        'price' => 8,
+        'count' => 4
+    ],
+];
+sort($arr);
+foreach ($arr as $key => $value){
+    foreach ($value as $k => $v) {
+//        echo $k . ' - ' . $v . "</br>";
+      /*  print_r($value);
+        echo "</br>";*/
+        echo ($key+1).' - '. $k . " = " . $v . "\n";
+        echo "</br>";
+    }
+}
+
+
+?>
