7b24e8a4d47b02d99b20701c0e7eebc4e0284035
VyacheslavGL <48326907+VyacheslavGL@users.noreply.github.com>
2019-06-05 17:22:38 +0300

    change file

--- HW_5/part 3/form_handler.php
+++ HW_5/part 3/form_handler.php
@@ -12,116 +12,27 @@ $size = $file['picture']['size'];
 var_dump($size);
 
 function type_size($type, $size){
-    for ($i = 0; $i<count($type); $i++){
+    $count = count($type);
+    for ($i = 0; $i<$count; $i++){
         $exp = explode('/', $type[$i]);
-        if ($exp[0] === 'image' && size($size)){
-            echo "Файл добавлен $exp[0] - $size[$i] </br>";
+        if ($exp[0] === 'image' && size($size[$i])){
+            echo "Файл добавлен $exp[1] - $size[$i] </br>";
         }else{
-            echo "Файл не добавлен $exp[0] - $size[$i]</br>";
+            echo "Файл не добавлен $exp[1] - $size[$i]</br>";
         }
     }
 }
 
 function size($size){
-        for ($i=0; $i<count($size); $i++){
-            if ($size[$i] <= 150000) {
-//                var_dump($size[$i]);
-//                echo "Файл добавлен</br>";
+            if ($size <= 150000) {
                 return true;
             } else {
-//                var_dump($size[$i]);
-//                echo "Файл не добавлен size</br>";
                 return false;
             }
-        }
 
-}
-
-
-type_size($type, $size);
-//size($size);
-
-
-
-
-
-/*$name = $file['picture']['name'];
-//var_dump($name);
-
-//echo "type";
-$type = $file['picture']['type'];
-//var_dump($type);
-
-$size = $file['picture']['size'];*/
-//var_dump($size);
-//echo "explode";
-//$explode = explode('/', $type);
-
-//var_dump($explode[0]);
-
-/*echo "explode foreach";
-foreach ($explode as $value){
-    var_dump($value);
-}*/
-
-/*echo "type size </br>";
-
-function type_size($type, $size){
-    foreach ($type as $key => $value){
-        $exp = explode('/', $value);
-        foreach ($exp as $value){
-            if ($value == 'image'){
-//            echo "Файл добавлен</br>";
-                size($size);
-            }
-        }
-    }
-}
-
-function size($size){
-    foreach ($size as $key => $value){
-      if($value < 150000){
-          echo "Файл $value добавлен</br>";return;
-      }else{
-          echo "Файлы не добавлены</br>";return;
-      }
-    }
-}
-
-type_size($type, $size);*/
-//size($size);
-
-
-//echo "type size </br>";
-/*
-function type_size($type, $size){
-    foreach ($type as $key => $value){
-        $exp = explode('/', $value);
-        var_dump($value);
-        foreach($exp as $key1=>$value1){
-            var_dump($value1);
-            if (($value1 == 'image') && size($size)){
-                echo "Файл добавлен</br>";
-            } else{
-                echo "Файл не добавлен size</br>";
-            }
-        }
-
-    }
-}
-
-//echo "Файл не добавлен size</br>";
-
-function size($size){
-    foreach ($size as $key => $value){
-        //var_dump($value);
-        if($value < 150000){
-            return true;
-        }else{
-            return false;
-        }
-    }
 }
 
 type_size($type, $size);
-//size($size);*/
+
+
+
