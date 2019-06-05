4e798686932200985ae6def1e415b38e2d5c5363
VyacheslavGL <48326907+VyacheslavGL@users.noreply.github.com>
2019-06-05 13:22:05 +0300

    add

--- HW_5/part 3/form_handler.php
+++ HW_5/part 3/form_handler.php
@@ -5,15 +5,46 @@ $file = $_FILES;
 
 var_dump($file);
 
-$name = $file['picture']['name'];
+$type = $file['picture']['type'];
+var_dump($type);
+
+$size = $file['picture']['size'];
+var_dump($size);
+
+function type_size($type, $size){
+    for ($i = 0; $i<count($type); $i++){
+        $exp = explode('/', $type[$i]);
+        if ($exp[0] === 'image' && size($size)){
+            echo "Файл добавлен</br>";
+        }else{
+            echo "Файл не добавлен size</br>";
+        }
+    }
+}
+
+function size($size)
+{
+    foreach ($size as $key => $value) {
+        var_dump($value);
+        if ($value <= 150000) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+type_size($type, $size);
+
+/*$name = $file['picture']['name'];
 //var_dump($name);
 
 //echo "type";
 $type = $file['picture']['type'];
 //var_dump($type);
 
-$size = $file['picture']['size'];
-var_dump($size);
+$size = $file['picture']['size'];*/
+//var_dump($size);
 //echo "explode";
 //$explode = explode('/', $type);
 
@@ -53,16 +84,20 @@ type_size($type, $size);*/
 
 
 //echo "type size </br>";
-
+/*
 function type_size($type, $size){
     foreach ($type as $key => $value){
         $exp = explode('/', $value);
-        var_dump($exp[0]);
-            if (($exp[0] == 'image') && size($size)){
-                    echo "Файл добавлен</br>";
+        var_dump($value);
+        foreach($exp as $key1=>$value1){
+            var_dump($value1);
+            if (($value1 == 'image') && size($size)){
+                echo "Файл добавлен</br>";
             } else{
                 echo "Файл не добавлен size</br>";
-                }
+            }
+        }
+
     }
 }
 
@@ -70,7 +105,7 @@ function type_size($type, $size){
 
 function size($size){
     foreach ($size as $key => $value){
-        var_dump($value);
+        //var_dump($value);
         if($value < 150000){
             return true;
         }else{
@@ -80,4 +115,4 @@ function size($size){
 }
 
 type_size($type, $size);
-//size($size);
+//size($size);*/
